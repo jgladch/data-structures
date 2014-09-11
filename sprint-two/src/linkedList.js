d443f34875dca7df6ac5f847a1e66c5d8782f93e
jgladch <jeffgladchun@gmail.com>
2014-09-11 16:24:40 -0700

    Some tests passing

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
+    return;
   };
 
   list.removeHead = function(){
+    //save current head (var x) so we can return it
+    var returnHead = list.head;
+    //move head to where the current head's next value points to
+    list.head = list.head.next;
+    //return var x
+    return returnHead.value;
   };
 
   list.contains = function(target){
+    //starting with list.head.valu
+    // start at head until we get to a tail that is equal to null
+    // for each node, check if the value === target
+    // if it is equal to the target, return true
+    // else return false
+
+    var currentNode = list.head;
+    // var result = false;
+    while (currentNode.next !== null){
+      if (currentNode.value === target){
+        // result = true;
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+
   };
 
   return list;
