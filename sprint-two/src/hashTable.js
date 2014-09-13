1e0d8d56be233485ebe18d743f411ce3e62d1eca
jgladch <jeffgladchun@gmail.com>
2014-09-13 12:17:36 -0700

    Completed reindexing hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,18 +1,51 @@
 var HashTable = function(){
+  this._count = 0;
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+};
+
+HashTable.prototype.reindex = function(reindexType) {
+  var oldValues = [];
+
+  this._storage.each(function(value, key, collection) {
+    if (value !== undefined) {
+      for (var x = 0; x < value.length; x++) {
+        oldValues.push(value[x]);
+      }
+      value = undefined;
+    }
+  });
+
+  if (reindexType === 'increase'){
+    var newLimit = this._limit * 2;
+  } else if (reindexType === 'decrease') {
+    var newLimit = this._limit / 2;
+  }
+  this._limit = newLimit;
+  this._storage = makeLimitedArray(newLimit);
+
+  for (var y = 0; y < oldValues.length; y++) {
+    var i = getIndexBelowMaxForKey(oldValues[y][0], this._limit);
+    this._storage.set(i, [ oldValues[y][0], oldValues[y][1] ]);
+  }
+
 };
 
 // O(1)
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  //check if there is an entry in the hash index already
+
   if (this._storage.get(i) !== undefined) {
     var currentHash = this._storage.get(i);
     currentHash.push([k,v]);
     this._storage.set(i,currentHash);
-  } else { //nothing in the hash index yet
+  } else {
     this._storage.set(i,[[k,v]]);
+  }
+  this._count += 1;
+
+  if (this._count / this._limit >= 0.75) {
+    this.reindex('increase');
   }
 };
 
@@ -45,9 +78,12 @@ HashTable.prototype.remove = function(k){
   } else {
     target.splice(0,1);
   }
+  this._count -= 1;
+
+  if (this._count / this._limit < 0.25 && this._limit > 8) {
+    this.reindex('decrease');
+  }
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
