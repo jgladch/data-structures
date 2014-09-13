c788b1a858366f0d6e7f163c3fea7acd3f11eea2
jgladch <jeffgladchun@gmail.com>
2014-09-13 10:33:56 -0700

    Refactored 'nodes' to be an object

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,5 +1,5 @@
 var Graph = function(){
-  this.nodes = []; //may be []?
+  this.nodes = {};
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
@@ -7,89 +7,97 @@ Graph.prototype.addNode = function(newNode, toNode){
     value: newNode,
     edges: []
   };
-  if (this.nodes.length === 1) {
-    newVertex.edges.push(this.nodes[0]);
-    this.nodes[0].edges.push(newVertex);
+
+  var  keys = Object.keys(this.nodes);
+
+  if (keys.length === 1){
+    newVertex.edges.push(this.nodes[keys[0]]);
+    this.nodes[keys[0]].edges.push(newVertex);
   }
+
   if (toNode !== undefined) {
-    for (var x = 0; x < this.nodes.length; x++){
-      if (this.nodes[x].value === toNode) {
-        newVertex.edges.push(this.nodes[x]);
-      }
-    }
+    newVertex.edges.push(this.nodes[toNode]);
   }
-  this.nodes.push(newVertex);
+  this.nodes[newNode] = newVertex;
 };
 
 Graph.prototype.contains = function(node){
-  var result = false;
-  for (var x = 0; x < this.nodes.length; x++){
-    if (this.nodes[x].value === node){
-      result = true;
-    }
-  }
-  return result;
+  // if ( this.nodes[node] !== undefined ) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return (this.nodes[node] !== undefined) ? true : false;
 };
 
 Graph.prototype.removeNode = function(node){
   //splice will cause a skipped index
-  for (var x = 0; x < this.nodes.length; x++) {
-    if (this.nodes[x].value === node) {
-      this.nodes.splice(x,1);
+  var edgeNodes = this.nodes[node].edges;
+
+  // for (var x = 0; x < this.nodes.length; x++) {
+  //   if (this.nodes[x].value === node) {
+  //     this.nodes.splice(x,1);
+  //   }
+  // }
+
+  for (var x = 0; x < edgeNodes.length; x++){
+    for (var y = 0; y < edgeNodes[x].edges.length; y++){
+      if ( edgeNodes[x].edges[y] === this.nodes[node]) {
+        edgeNodes[x].edges.splice(y,1);
+      }
     }
   }
+  this.nodes[node] = undefined;
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
   var result = false;
-  for (var y = 0; y < this.nodes.length; y++) {
-    if (this.nodes[y].value === fromNode) {
-      var originNode = this.nodes[y];
-    }
-  }
-  for (var x = 0; x < originNode.edges.length; x++) {
-    if (originNode.edges[x].value === toNode) {
+  for (var x = 0; x < this.nodes[fromNode].edges.length; x++) {
+    if (this.nodes[fromNode].edges[x] === this.nodes[toNode]) {
       result = true;
     }
   }
   return result;
+  // var result = false;
+  // for (var y = 0; y < this.nodes.length; y++) {
+  //   if (this.nodes[y].value === fromNode) {
+  //     var originNode = this.nodes[y];
+  //   }
+  // }
+  // for (var x = 0; x < originNode.edges.length; x++) {
+  //   if (originNode.edges[x].value === toNode) {
+  //     result = true;
+  //   }
+  // }
+  // return result;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  var from;
-  var to;
-  for (var x = 0; x < this.nodes.length; x++){
-    if (this.nodes[x].value === fromNode) {
-      from = this.nodes[x];
-    }
-  }
-  for (var z = 0; z < this.nodes.length; z++){
-    if (this.nodes[z].value === toNode) {
-      to = this.nodes[z];
-    }
-  }
+  var from = this.nodes[fromNode];
+  var to = this.nodes[toNode];
+
   from.edges.push(to);
   to.edges.push(from);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  var from;
-  var to;
-  for (var x = 0; x < this.nodes.length; x++){
-    if (this.nodes[x].value === fromNode) {
-      from = this.nodes[x];
-    }
-  }
-  for (var z = 0; z < this.nodes.length; z++){
-    if (this.nodes[z].value === toNode) {
-      to = this.nodes[z];
-    }
-  }
+  var from = this.nodes[fromNode];
+  var to = this.nodes[toNode];
+  // for (var x = 0; x < this.nodes.length; x++){
+  //   if (this.nodes[x].value === fromNode) {
+  //     from = this.nodes[x];
+  //   }
+  // }
+  // for (var z = 0; z < this.nodes.length; z++){
+  //   if (this.nodes[z].value === toNode) {
+  //     to = this.nodes[z];
+  //   }
+  // }
   for (var y = 0; y < from.edges.length; y++){
-    if (from.edges[y].value === toNode) { from.edges.splice(y,1) }
+    if (from.edges[y] === to) { from.edges.splice(y,1) }
   }
   for (var s = 0; s < to.edges.length; s++){
-    if (to.edges[s].value === fromNode) { to.edges.splice(s,1) }
+    if (to.edges[s] === from) { to.edges.splice(s,1) }
   }
   if (from.edges.length === 0){
     this.removeNode(fromNode);
